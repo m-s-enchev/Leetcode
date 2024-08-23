4f32dba7cbe447fd22b8a877d8f7b9abb96eed32
m-s-enchev <116597416+m-s-enchev@users.noreply.github.com>
2024-08-23 23:24:18 +0300

    Add files via upload

---
+++ Valid_Parentheses.py
@@ -0,0 +1,37 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+#
+#
+# Example 2:
+#
+# Input: s = "()[]{}"
+# Output: true
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+
+
+def is_valid(s):
+    symbol_map = {'(': ')', '[': ']', '{': '}'}
+    for i in range(len(s)):
+        opening = s[i]
+        if opening in symbol_map.keys():
+            closing = symbol_map[opening]
+            index = s.find(closing)
+            gap = index - i
+            if index < 0 or (gap > 0 and gap % 2 == 0):
+                return False
+    return True
+
+
+print(is_valid("(]"))
