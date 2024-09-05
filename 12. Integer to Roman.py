662b2a02b9dd6e07ff82b212d5345b6816aec36d
m-s-enchev <m.s.enchev@gmail.com>
2024-09-05 22:58:30 +0300

    Integer to roman

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -41,12 +41,21 @@
 
 
 def intToRoman(num):
+    number_list = list(str(num))
     answer_in_roman = ""
-    decimal_order = {1: {1:'I', 5:'V', 10:'X'}, 2: {10:"X", 50:'L', 100:'C'}, 3: { 100:'C', 500:'D', 1000:'M'}, 4:{1000:'M'}}
-    for i in range(len(num)-1,0,-1):
-        numbers_dict=decimal_order[len(num)-i]
-        for el in numbers_dict.keys():
-            if num[i] == el:
-                answer_in_roman = numbers_dict[el] + answer_in_roman
-                break
-            elif
+    decimal_order = {
+        1:{1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6: 'VI', 7:'VII', 8:'VIII', 9: 'IX', 10:'X'},
+        2:{10:'X', 20:'XX', 30:'XXX', 40:'XL', 50:'L', 60: 'LX', 70:'LXX', 80:'LXXX', 90: 'XC', 100:'C'},
+        3:{100:'C', 200:'CC', 300:'CCC', 400:'CD', 500:'D', 600: 'DC', 700:'DCC', 800:'DCCC', 900: 'CM', 1000:'M'},
+        4:{1000:'M'}
+                     }
+    for i in range(len(str(num))-1,,-1):
+        numbers_dict=decimal_order[len(str(num))-i]
+        for key in numbers_dict.keys():
+            if int(number_list[i]) == key:
+                answer_in_roman = numbers_dict[key] + answer_in_roman
+    return answer_in_roman
+
+print(intToRoman(58))
+
+
