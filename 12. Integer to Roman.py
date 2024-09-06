8dbc8a36a96a37b466adfb22bccc04249851a642
m-s-enchev <m.s.enchev@gmail.com>
2024-09-06 09:15:05 +0300

    Integer to roman Solution 1

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -41,21 +41,22 @@
 
 
 def intToRoman(num):
-    number_list = list(str(num))
+    list_from_number = list(str(num))
     answer_in_roman = ""
-    decimal_order = {
+    decimal_order_dict = {
         1:{1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6: 'VI', 7:'VII', 8:'VIII', 9: 'IX', 10:'X'},
         2:{10:'X', 20:'XX', 30:'XXX', 40:'XL', 50:'L', 60: 'LX', 70:'LXX', 80:'LXXX', 90: 'XC', 100:'C'},
         3:{100:'C', 200:'CC', 300:'CCC', 400:'CD', 500:'D', 600: 'DC', 700:'DCC', 800:'DCCC', 900: 'CM', 1000:'M'},
-        4:{1000:'M'}
+        4:{1000:'M', 2000:'MM', 3000:'MMM'}
                      }
-    for i in range(len(str(num))-1,,-1):
-        numbers_dict=decimal_order[len(str(num))-i]
+    for i in range(len(list_from_number)-1,-1,-1):
+        decimal_position = len(list_from_number)-i
+        numbers_dict = decimal_order_dict[decimal_position]
         for key in numbers_dict.keys():
-            if int(number_list[i]) == key:
+            if int(list_from_number[i])*(10**(decimal_position-1)) == key:
                 answer_in_roman = numbers_dict[key] + answer_in_roman
     return answer_in_roman
 
-print(intToRoman(58))
+print(intToRoman(1994))
 
 
